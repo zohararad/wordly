d93980e8699675ad90991d7ab9a6e7e0e1e92d37
Zohar Arad <zohar@zohararad.com>
2012-07-06 17:49:05 +0300

    added published and by_category scopes for Post model and support for posts by category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,12 @@ class PostsController < ApplicationController
 
   def index
     page = params[:page] || 1
-    @posts = Post.where(:published => 1).page(page).per(global_settings.posts_per_page)
+    if params[:category_id].nil?
+      @posts = Post.published.page(page).per(global_settings.posts_per_page)
+    else
+      @category = Category.find_by_slug(params[:category_id])
+      @posts = Post.in_category(@category.id).page(page).per(global_settings.posts_per_page)
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :categories
 
+  # published posts
+  scope :published, where(:published => 1).includes(:categories, :author)
+
+  # posts in category
+  scope :in_category, lambda {|category_id|
+    where(["posts.published = 1 and categories_posts.category_id = ?", category_id]).joins(:categories, :author).includes(:categories, :author)
+  }
+
   private
 
   def set_slug
