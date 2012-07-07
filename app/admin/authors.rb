8fc9a07ff44c50cc18fc5e2b3b3f230285d1e5c3
Zohar Arad <zohar@zohararad.com>
2012-07-07 13:51:28 +0300

    fixed authors edit page - added password editing support

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -7,10 +7,22 @@ ActiveAdmin.register Author do
       f.input :email, :as => :email
       f.input :website, :as => :url
       f.input :about
-      #f.input :password
-      #f.input :password_confirmation
+      f.input :password
+      f.input :password_confirmation
     end
     f.buttons
+  end
+
+  controller do
+
+    def update
+      if params[:author][:password].blank?
+        params[:author].delete(:password)
+        params[:author].delete(:password_confirmation)
+      end
+      super
+    end
+
   end
 
 end
