00b1517085300f43377414ccc8c0b6d7a388866f
Zohar Arad <zohar@zohararad.com>
2012-07-21 16:32:14 +0300

    added Page model validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,10 @@ class Page < ActiveRecord::Base
   attr_protected :author_id
 
   belongs_to :author
+
+  validates :title,       :presence => true, :length => { :minimum => 2 }
+  validates :slug,        :presence => true, :uniqueness => true
+  validates :content,     :presence => true, :length => { :minimum => 2 }
+  validates :author_id,   :presence => true
+
 end
