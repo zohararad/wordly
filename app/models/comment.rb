dabd8ee705b1fecff7d80b21c86a7367237c5d3a
Zohar Arad <zohar@zohararad.com>
2012-07-21 16:27:13 +0300

    added activemodel validations to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ActiveRecord::Base
 
   belongs_to :post
   belongs_to :author
+
+  validates :post_id,          :presence => true
+  validates :author_name,      :presence => true
+  validates :comment,          :presence => true
+  validates :author_email,     :allow_blank => true, :email => true
+  validates :author_website,   :allow_blank => true, :uri => true
+
 end
