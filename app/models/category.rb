219439ac114f5b926a662380a00ff86d0addea42
Zohar Arad <zohar@zohararad.com>
2012-07-21 16:27:26 +0300

    organize model validators and relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many :posts
-
   attr_accessible :name, :slug
+
+  has_and_belongs_to_many :posts
 
   validates :name,    :presence => true,  :length => { :minimum => 2 }, :uniqueness => true
   validates :slug,    :presence => true,  :length => { :minimum => 2 }, :uniqueness => true
