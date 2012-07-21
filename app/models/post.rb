ee5f789c648ebd0f87d38bbf3b9e3f57dcc645fb
Zohar Arad <zohar@zohararad.com>
2012-07-21 16:12:38 +0300

    updated post spec - added post excerpt handling in case excerpt is empty

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+  include ActionView::Helpers::SanitizeHelper
+
   attr_protected :author_id
   acts_as_taggable_on :tags
   before_save :set_slug
@@ -6,6 +9,11 @@ class Post < ActiveRecord::Base
   belongs_to :author
   has_many :comments
   has_and_belongs_to_many :categories
+
+  validates :title,    :presence => true,  :length => { :minimum => 2 }
+  validates :content,  :presence => true,  :length => { :minimum => 10 }
+  validates :excerpt,                      :length => { :minimum => 2 }, :allow_nil => true
+  validates :slug,     :uniqueness => true, :allow_nil => false
 
   # published posts
   scope :published, where(:published => 1).includes(:categories, :author)
@@ -18,6 +26,11 @@ class Post < ActiveRecord::Base
   scope :latest_by_author, lambda { |author_id|
     where(['author_id = ?', author_id]).order('created_at desc').limit(10)
   }
+
+  def post_excerpt
+    excerpt || strip_tags(content.gsub(/<\/([^>]+)>/,"</$1> ")).split(' ')[0..30].join(' ') + '...'
+  end
+
   private
 
   def set_slug
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Post do
 
   before(:all) do
-    @post = Post.new(:excerpt => 'test', :content => 'some text')
+    @post = Post.new(:content => '<h1>Some Title</h1><p>some text <strong>again</strong></p>')
     @post.author_id = 10
+  end
+
+  it 'should return shortened post content if excerpt is nil' do
+    @post.post_excerpt.should == 'Some Title some text again...'
   end
 
   it 'should not have a slug if title is nil and post is not saved' do
