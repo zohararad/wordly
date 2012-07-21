23ee0e3a192ed5ccb6d31c861e62d5e519662f1c
Zohar Arad <zohar@zohararad.com>
2012-07-21 15:53:36 +0300

    added post specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @post = Post.new(:excerpt => 'test', :content => 'some text')
+    @post.author_id = 10
+  end
+
+  it 'should not have a slug if title is nil and post is not saved' do
+    @post.slug.should == nil
+  end
+
+  it 'should have a slug after setting title and saving' do
+    @post.title = 'Some Test Title'
+    @post.save
+    @post.slug.should == 'some-test-title'
+  end
+
 end
