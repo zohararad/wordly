2a3d5dc6e2f04b40b6b92f5a450f3f9260061528
Zohar Arad <zohar@zohararad.com>
2012-07-21 16:22:16 +0300

    added setting spec

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Setting do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @setting = Setting.new :configuration => Setting::DEFAULT_SETTINGS
+  end
+
+  it 'should have default getters with values from Setting::DEFAULT_SETTINGS' do
+    Setting::DEFAULT_SETTINGS.each do |k, v|
+      @setting.send(k.to_sym).should == v
+    end
+  end
+
+  it 'should have default setters from Setting::DEFAULT_SETTINGS' do
+    Setting::DEFAULT_SETTINGS.keys.each do |k|
+      @setting.respond_to?("#{k}=").should == true
+    end
+  end
+
+  it 'should set value in configuration hash when calling setter' do
+    @setting.site_name = 'Some Name'
+    @setting.configuration[:site_name].should == 'Some Name'
+  end
+
 end
