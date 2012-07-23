0f4e3cc2a685b827663eb035a64444d8de89f642
Zohar Arad <zohar@zohararad.com>
2012-07-23 19:41:06 +0300

    added method documentation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,31 +24,42 @@ class Post < ActiveRecord::Base
     where(["posts.published = 1 and categories_posts.category_id = ?", category_id]).joins(:categories, :author).includes(:categories, :author)
   }
 
+  # Latest posts by author
   scope :latest_by_author, lambda { |author_id|
     where(['author_id = ?', author_id]).order('created_at desc').limit(10)
   }
 
+  # Returns either the post's excerpt or a stripped version of the post's content in case the excerpt is blank
+  # @return [String] excerpt or stripped version of post content
   def post_excerpt
-    excerpt || strip_tags(content.gsub(/<\/([^>]+)>/,"</$1> ")).split(' ')[0..30].join(' ') + '...'
+    excerpt.blank? ? strip_tags(content.gsub(/<\/([^>]+)>/,"</$1> ")).split(' ')[0..30].join(' ') + '...' : excerpt
   end
 
+  # Generates a uid from a Base64 encoded version of id and a random salt
+  # @return [String] uid
   def uid_from_id
     salt = rand(36**8).to_s(36)
     str = [salt, self.id].join('')
     Base64::encode64(str).gsub(/\n/,'')
   end
 
+  # Decodes uid and extracts that record id part of the decoded string
+  # @param [String] uid - uid to decode
+  # @return [Interger] record id
   def self.id_from_uid(uid)
     Base64::decode64(uid)[8..-1].to_i
   end
 
   private
 
+  # Sets a unique id to a post that is used to obfuscate the record's id in forms and urls
+  # Called after record creation
   def set_uid
     self.uid = uid_from_id
     self.save
   end
 
+  # Sets the post's slug to a parameterized version of the title in case the slug is blank
   def set_slug
     self.slug = self.title.downcase.parameterize if self.slug.blank?
   end
